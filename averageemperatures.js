1fae6472d63039b4b8e564d451b9abfb7dfee62d
mdnasir20 <mdnasir.fi@gmail.com>
2022-06-06 12:26:12 +0300

    first commit

---
+++ averageemperatures.js
@@ -0,0 +1,12 @@
+
+var temperatures = [-3.3, -4.7, -1.3, 3.9, 10.2, 14.6, 17.8,
+				16.3, 11.5, 6.6, 1.6, -2.0];
+
+function loopTemperatures() {
+    
+       for(let i = 0; i < temperatures.length; i++) {
+	
+console.log(i+1+".",temperatures[i].toFixed(1));
+}  
+}  
+  
